45d63bcd2497b41c12881d4cf941563c398f46b8
Abagiel <66873762+Abagiel@users.noreply.github.com>
2020-09-21 14:01:28 +0300

    added board to charts and minor fixes

--- canvent/components/Chart/Chart.js
+++ canvent/components/Chart/Chart.js
@@ -1,6 +1,8 @@
 import { DomListener } from '../DomListener/DomListener';
 import { CanventText } from '../Text/Text';
 
+import { capitalize } from '../tools/utils';
+
 export class CanventChart {
 	constructor(ctx, data, settings) {	
 		this.ctx = ctx;
@@ -15,42 +17,34 @@ export class CanventChart {
 	}
 
 	create() {
-		switch(this.settings.type) {
-			case 'pie':
-				this.data.forEach(this.createPie);
-				break;
-			case 'doughnut':
-				this.data.forEach(this.createDoughnut);
-				break;
-			case 'vertical':
-				this.data.forEach(this.createVerticalChart);
-				break;
-			case 'horizontal':
-				this.data.forEach(this.createHorizontalChart);
-				break;
-			default: return;
-		}
+		const type = capitalize(this.settings.type);
+		this.data.forEach(this[`create${type}Chart`])
 	}
 
-	createPie = (data) => {
+	createPieChart = (data, i) => {
 		const { fill, strokeColor, value } = data;
-		const { x, y, r, line } = this.settings;
+		const { x, y, r, h, w, line, board } = this.settings;
+		const color = strokeColor || this.settings.strokeColor || '#000';
 		this.endPoint += Math.PI * 2 / this.wholeArc * value;
 
 		this.ctx.beginPath();
 		this.ctx.fillStyle = fill || this.settings.fill;
-		this.ctx.lineWidth = line;
-		this.ctx.strokeStyle = strokeColor || this.settings.strokeColor || '#000';
-		this.ctx.moveTo(x, y);
-		this.ctx.arc(x, y, r, this.startPoint, this.endPoint);
-		this.ctx.lineTo(x, y);
+		this.ctx.moveTo(x + w / 2, y + h / 2);
+		this.ctx.arc(x + w / 2, y + h / 2, r, this.startPoint, this.endPoint);
+		this.ctx.lineTo(x + w / 2, y + h / 2);
 		this.ctx.fill()
 		if (this.settings.stroke) {
-			this.ctx.stroke()
+			stroke(this.ctx, color, line);
 		}
 		this.ctx.closePath();
 
 		this.startPoint = this.endPoint;
+
+		if (i === this.data.length - 1) {
+			composite(this.ctx, 'destination-atop');
+			fillRect(this.ctx, x, y, w, h, this.settings.fill);
+		}
+
 	}
 
 	calcValues() {
@@ -59,13 +53,14 @@ export class CanventChart {
 		})
 	}
 
-	createDoughnut = (data) => {
-		const { x, y, holeSize, holeFill } = this.settings;
+	createDoughnutChart = (data, i) => {
+		const { x, y, w, h, holeSize, holeFill } = this.settings;
 
-		this.createPie(data);
+		this.createPieChart(data, i);
 		this.ctx.beginPath();
+		composite(this.ctx, 'source-over');
 		this.ctx.fillStyle = holeFill;
-		this.ctx.arc(x, y, holeSize, 0, 2 * Math.PI);
+		this.ctx.arc(x + w / 2, y + h / 2, holeSize, 0, 2 * Math.PI);
 		this.ctx.fill();
 		if (this.settings.stroke) {
 			this.ctx.stroke();
@@ -76,6 +71,7 @@ export class CanventChart {
 	createVerticalChart = (data, i) => {
 		const { fill, strokeColor, value } = data;
 		const { x, y, h, w, gap = 0, line = 2 } = this.settings;
+		const color = fill || this.settings.fill;
 		const length = this.data.length;
 		const rectH = h / this.wholeArc * value;
 		const rectW = (w - line - gap * (length - 1)) / length;
@@ -83,20 +79,15 @@ export class CanventChart {
 		const rectY = y + h - rectH - line / 2;
 
 		this.ctx.beginPath();
-		this.ctx.lineWidth = line;
-		this.ctx.strokeStyle = this.settings.strokeColor;
 		this.ctx.rect(x, y, w, h);
-		this.ctx.stroke()
+		stroke(this.ctx, this.settings.strokeColor, line);
 		this.ctx.closePath();
 
 		this.ctx.beginPath();
-		this.ctx.fillStyle = fill || this.settings.fill;
-		this.ctx.rect(rectX, rectY, rectW, rectH);
-		this.ctx.fill();
+		fillRect(this.ctx, rectX, rectY, rectW, rectH, color);
 
 		if (this.settings.stroke) {
-			this.ctx.strokeStyle = strokeColor;
-			this.ctx.stroke();
+			stroke(this.ctx, strokeColor, line);
 		}
 		this.ctx.closePath();
 	}
@@ -104,6 +95,7 @@ export class CanventChart {
 	createHorizontalChart = (data, i) => {
 		const { fill, strokeColor, value } = data;
 		const { x, y, h, w, gap = 0, line = 2 } = this.settings;
+		const color = fill || this.settings.fill;
 		const length = this.data.length;
 		const rectW = w / this.wholeArc * value;
 		const rectH = (h - line - gap * (length - 1)) / length;
@@ -111,21 +103,36 @@ export class CanventChart {
 		const rectX = x + line / 2;
 
 		this.ctx.beginPath();
-		this.ctx.lineWidth = line;
-		this.ctx.strokeStyle = this.settings.strokeColor;
 		this.ctx.rect(x, y, w, h);
-		this.ctx.stroke()
+		stroke(this.ctx, this.settings.strokeColor, line);
 		this.ctx.closePath();
 
 		this.ctx.beginPath();
-		this.ctx.fillStyle = fill || this.settings.fill;
-		this.ctx.rect(rectX, rectY, rectW, rectH);
-		this.ctx.fill();
+		fillRect(this.ctx, rectX, rectY, rectW, rectH, color);
 
 		if (this.settings.stroke) {
-			this.ctx.strokeStyle = strokeColor;
-			this.ctx.stroke();
+			stroke(this.ctx, strokeColor, line);
 		}
 		this.ctx.closePath();
 	}
+}
+
+function stroke(ctx, color, width) {
+	ctx.lineWidth = width;
+	ctx.strokeStyle = color;
+	ctx.stroke();
+}
+
+function fillRect(ctx, x, y, w, h, color) {
+	ctx.fillStyle = color;
+	ctx.rect(x, y, w, h);
+	ctx.fill();
+}
+
+function combine(...args) {
+	args.forEach(arg => arg());
+}
+
+function composite(ctx, type) {
+	ctx.globalCompositeOperation = type;
 }
--- canvent/components/tools/utils.js
+++ canvent/components/tools/utils.js
@@ -3,3 +3,7 @@ export const toMethodName = (str) => `${str}Handler`;
 export function changeCursor(type) {
 	document.body.style.cursor = type;
 }
+
+export function capitalize(str) {
+	return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+}
